95af0d28c6e32ebbc7ab26d5aea46e25a15e7ff1
Jonas Auer <jonas.auer.94@gmail.com>
2018-08-15 15:17:26 +0200

    Start REST server along with MQTT client

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 'use strict'
 
+const log = require('./lib/util/logger').createLogger(__filename)
+
+const { Server } = require('./lib/server/server')
+const { TurtleManager } = require('./lib/models/turtle-manager')
 const { Client } = require('./lib/mqtt-client/client')
-const client = new Client()
-client.run()
+
+const turtleManager = new TurtleManager()
+const client = new Client(turtleManager, {
+  server: process.env.MQTT_SERVER,
+  baseTopic: process.env.MQTT_BASE_TOPIC
+})
+const server = new Server(client, turtleManager)
+
+for (let signal of ['SIGINT', 'SIGQUIT', 'SIGTERM']) {
+  process.on(signal, () => {
+    log.info(`Received ${signal}, shutting down`)
+    client.stop()
+    server.stop()
+  })
+}
+
+Promise.all([
+  client.run(),
+  server.listen(3000)
+])
